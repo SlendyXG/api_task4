8c2b6afa41e03a86846c58eede90378fd8b223f6
SlendyXG <105538051+SlendyXG@users.noreply.github.com>
2026-02-12 01:36:12 +0300

    Update space_images_bot.py

--- space_images_bot.py
+++ space_images_bot.py
@@ -3,9 +3,11 @@ import os
 import telegram
 from dotenv import load_dotenv
 
+
 def main():
-    bot = telegram.Bot(token='8477642139:AAFNTj_ybZgT4SGfYEFasnLRb00zrOYGsWo')
     load_dotenv()
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    bot = telegram.Bot(token=telegram_bot_token)
     chat_id = os.environ['TELEGRAM_CHAT_ID']
     bot.send_message(chat_id=chat_id, text="Проверка отправки сообщений")
 
