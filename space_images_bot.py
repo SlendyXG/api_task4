427450c280b152fa3d9fa068119d461f529adb23
SlendyXG <105538051+SlendyXG@users.noreply.github.com>
2026-02-12 00:33:32 +0300

    publish photo to Telegram channel.

--- space_images_bot.py
+++ space_images_bot.py
@@ -3,11 +3,12 @@ import os
 import telegram
 from dotenv import load_dotenv
 
+
 def main():
     bot = telegram.Bot(token='8477642139:AAFNTj_ybZgT4SGfYEFasnLRb00zrOYGsWo')
     load_dotenv()
     chat_id = os.environ['TELEGRAM_CHAT_ID']
-    bot.send_message(chat_id=chat_id, text="Проверка отправки сообщений")
+    bot.send_photo(chat_id=chat_id, photo=open('images/nasa_apod_2.jpg', 'rb'), timeout=60)
 
 
 if __name__ == '__main__':
