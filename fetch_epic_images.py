8963173038130295e7a191064b0db7e87ee6f751
SlendyXG <105538051+SlendyXG@users.noreply.github.com>
2026-02-26 21:31:53 +0300

    Update fetch_epic_images.py

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -1,6 +1,7 @@
 import os
 
 import requests
+from datetime import datetime
 
 
 def fetch_epic_photos():
@@ -16,17 +17,19 @@ def fetch_epic_photos():
 
     for image_number, image in enumerate(images[:5], start=1):
         image_name = image['image']
-        date_parts = image['date'].split()[0].replace('-', '/')
-        image_url = (f"https://epic.gsfc.nasa.gov/archive/natural/{date_parts}/png/{image_name}.png")
+        date = datetime.fromisoformat(image['date'].replace('Z', '+00:00'))
+        image_url = (f"https://epic.gsfc.nasa.gov/archive/natural/{date:%Y/%m/%d}/png/{image_name}.png")
         image_response = requests.get(image_url)
         image_response.raise_for_status()
-        filename = 'epic' + str(image_number) + '.png'
+        filename = f"epic{image_number}.png"
         file_path = os.path.join('images', filename)
-        with open(file_path, 'wb') as file:
+        with open(file_path, "wb") as file:
             file.write(image_response.content)
+
 
 def main():
     fetch_epic_photos()
+
 
 if __name__ == '__main__':
     main()
